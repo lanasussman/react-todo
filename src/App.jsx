7a23da702e2ccb8bd8c5f6b4f51991638ef6269a
lana <lana@lanas-MacBook-Pro.local>
2024-03-08 01:35:02 -0500

    lesson_1_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
 import "./App.css";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
-const todoList = [
-  {
-    id: 1,
-    title: "Review test cases for the new feature",
-  },
-  {
-    id: 2,
-    title: "Conduct regression testing on the latest build",
-  },
-  {
-    id: 3,
-    title: "Retest fixed bugs",
-  },
-];
 function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <div className="card">
-        <ul>
-          {todoList.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
-      </div>
+      <AddTodoForm />
+      <TodoList />
     </>
   );
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,29 @@
+
+const todoList = [
+  {
+    id: 1,
+    title: "Review test cases for the new feature",
+  },
+  {
+    id: 2,
+    title: "Conduct regression testing on the latest build",
+  },
+  {
+    id: 3,
+    title: "Retest fixed bugs",
+  },
+];
+
+function TodoList() {
+  return (
+    <div className="card">
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
