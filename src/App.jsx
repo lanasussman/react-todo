b0014f43f90b8833350c9b0aabcd3b039257bdc8
lana <lana@lanas-MacBook-Pro.local>
2024-04-29 00:05:58 -0400

    lesson_1_5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = localStorage.getItem("savedTodoList");
+    return savedTodoList ? JSON.parse(savedTodoList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-
+  const [todoList, setTodoList] = useSemiPersistentState();
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
