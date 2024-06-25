10f69aebe58b4aae640abc5484a84372faf69597
Lana N <80714766+lanasussman@users.noreply.github.com>
2024-06-25 10:50:38 -0400

    Lesson 4 1 (#11)
    * lesson_1_8
    
    * updated the removeTodo
    
    * lesson_1_9
    
    * lesson_3.1
    
    * lesson_4.1
    
    ---------
    
    Co-authored-by: lana <lana@lanas-MacBook-Pro.local>

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import InputWithLabel from "./InputWithLabel";
+import PropTypes from "prop-types";
 
 // eslint-disable-next-line react/prop-types
 function AddTodoForm({ onAddTodo }) {
@@ -26,5 +27,9 @@ function AddTodoForm({ onAddTodo }) {
     </form>
   );
 }
+
+AddTodoForm.propTypes = {
+  onAddTodo: PropTypes.func,
+};
 
 export default AddTodoForm;
